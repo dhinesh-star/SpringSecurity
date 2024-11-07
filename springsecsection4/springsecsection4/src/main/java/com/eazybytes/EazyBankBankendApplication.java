61f4ef9957a8360996376edc18a9ce378e20f984
dhinesh-star <dhineshmanickam2001@gmail.com>
2024-11-08 01:53:27 +0530

    User added from database

---
+++ springsecsection4/springsecsection4/src/main/java/com/eazybytes/EazyBankBankendApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBankendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBankendApplication.class, args);
+	}
+
+}
