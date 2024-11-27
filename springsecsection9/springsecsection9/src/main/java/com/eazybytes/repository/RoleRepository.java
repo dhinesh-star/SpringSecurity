6ac14377d85b189f919348df8609cbac0050210c
dhinesh-star <dhineshmanickam2001@gmail.com>
2024-11-28 00:57:52 +0530

    Authorization Changes Added

---
+++ springsecsection9/springsecsection9/src/main/java/com/eazybytes/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.eazybytes.repository;
+
+import com.eazybytes.model.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
